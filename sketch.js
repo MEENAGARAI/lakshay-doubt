41e95f7e590c66e041cb37f439b5c1f5676ed8ad
Meena Garai <68517659+MEENAGARAI@users.noreply.github.com>
2021-02-23 00:46:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var balloon, database;
+var position;
+
+
+
+function setup() {
+
+    database = firebase.database();
+    //console.log(database);
+    createCanvas(800, 400);
+
+
+    balloon = createSprite(400, 200, 50, 50);
+    balloon.shapeColor = "red"
+
+
+    var balloonPosition = database.ref('balloon/position');
+    balloonPosition.on("value", readPosition, showError);
+}
+
+
+function draw() {
+    background("white");
+
+    if (keyDown(LEFT_ARROW)) {
+        writePosition(-1, 0)
+    } else if (keyDown(RIGHT_ARROW)) {
+        writePosition(1, 0)
+    } else if (keyDown(UP_ARROW)) {
+        writePosition(0, -1)
+    } else if (keyDown(DOWN_ARROW)) {
+        writePosition(0, 1)
+    }
+
+    drawSprites();
+}
+
+function writePosition(x, y) {
+    database.ref('balloon/position').set({
+        'x': position.x + x,
+        'y': position.y + y
+    })
+}
+
+function readPosition(data) {
+    position = data.val();
+    //console.log(position.x);
+    balloon.x = position.x;
+    balloon.y = position.y;
+}
+
+function showError() {
+    console.log("Error in writing to the database");
+}
